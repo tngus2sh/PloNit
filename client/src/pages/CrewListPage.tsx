41f35335f295dd2639aaeaf557602bfb6dea6e2f
장혜원 <mins0104@naver.com>
2023-10-23 11:43:45 +0900

    [FEAT] custom_tab 설정

--- client/src/pages/CrewListPage.tsx
+++ client/src/pages/CrewListPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { BasicTopBar } from "components/common/TopBar";
+import CustomizedTabs from "components/common/CustomTab";
+import MyCrewList from "components/CrewList/MyCrewList";
+import TotalCrewList from "components/CrewList/TotalCrewList";
 
 const CrewListPage = () => {
+  const tabProps = {
+    나의크루목록: <MyCrewList />,
+    전체크루목록: <TotalCrewList />,
+  };
+
   return (
     <div>
       <BasicTopBar text="크루 목록" />
+      <CustomizedTabs tabProps={tabProps} />
     </div>
   );
 };
