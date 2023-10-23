211efff6f62345d430f48820aa3b271041ce6ae1
장혜원 <mins0104@naver.com>
2023-10-23 10:26:27 +0900

    [FEAT] 하단 nav_bar 레이아웃 설정

--- client/src/pages/CrewListPage.tsx
+++ client/src/pages/CrewListPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { BasicTopBar } from "components/common/TopBar";
 
 const CrewListPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BasicTopBar text="크루 목록" />
+    </div>
+  );
 };
 
 export default CrewListPage;
