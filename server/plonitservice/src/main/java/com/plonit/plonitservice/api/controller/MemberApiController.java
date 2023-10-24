e68cea2676b648d3387b39d44f211910872c8c27
소수현 <ssuhyeon98@gmail.com>
2023-10-24 14:37:06 +0900

    [CHORE] swagger config 추가 #S09P31C207-132

--- server/plonitservice/src/main/java/com/plonit/plonitservice/api/controller/MemberApiController.java
+++ server/plonitservice/src/main/java/com/plonit/plonitservice/api/controller/MemberApiController.java
@@ -1,5 +1,9 @@
 package com.plonit.plonitservice.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Tag(name = "Test", description = "설명")
 @RequestMapping("/plonit-service/api/member")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 public class MemberApiController {
     
+    @Operation(summary = "get test", description = "test설정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @GetMapping("/test")
     public String check(HttpServletRequest request) {
         log.info("server port = {}", request.getServerPort());
         return "Hi, there. This is a message from plonit-service";
     }
+    
 }
---
+++ server/plonitservice/src/main/java/com/plonit/plonitservice/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.plonit.plonitservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("")
+                .pathsToMatch("/plonit-service/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("PloNit API")
+                        .description("PloNit 프로젝트 API 명세서입니다.")
+                        .version("v0.0.1"));
+    }
+}
