bc5237afcaad93187272948c37c5135613735c3d
장혜원 <mins0104@naver.com>
2023-10-25 09:47:59 +0900

    [FEAT] 로그인 버튼 추가

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <img src="/kakao_login.png" alt="카카오" style={{ width: "90%" }} />
+    </div>
+  );
 };
 
 export default LoginPage;
