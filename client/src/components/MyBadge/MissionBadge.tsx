7fbc9cc4977bd038ffd1fa8b5f1f1179e95807eb
장혜원 <mins0104@naver.com>
2023-10-27 08:45:54 +0900

    [FEAT] 나의 뱃지 내부 뱃지 배열:

--- client/src/components/MyBadge/MissionBadge.tsx
+++ client/src/components/MyBadge/MissionBadge.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import style from "styles/css/MyBadgePage/MissionBadge.module.css";
 
 const MissionBadge = () => {
-  return <div>미션 뱃지</div>;
+  const images = [
+    { src: "/metamong.png", alt: "몽" },
+    { src: "/metamong.png", alt: "몽" },
+    { src: "/metamong.png", alt: "몽" },
+    { src: "/metamong.png", alt: "몽" },
+    { src: "/metamong.png", alt: "몽" },
+    { src: "/metamong.png", alt: "몽" },
+    { src: "/metamong.png", alt: "몽" },
+  ];
+
+  const renderImages = () => {
+    return images.map((image, index) => (
+      <div className={style.imageContainer} key={index}>
+        <img src={image.src} alt={image.alt} />
+      </div>
+    ));
+  };
+
+  return <div className={style.missionBadge}>{renderImages()}</div>;
 };
 
 export default MissionBadge;
