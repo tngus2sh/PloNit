1f849bc51749341e0024e3ce151649e4c955df5b
park_js515 <hdcom37@gmail.com>
2023-10-26 22:15:41 +0900

    [REFACTOR] 지도 함수 모듈화 및 보완

--- client/src/components/plogging/functions/useNaverMap.tsx
+++ client/src/components/plogging/functions/useNaverMap.tsx
@@ -1,7 +1,8 @@
 import { naver } from "components/common/useNaver";
-import { NavigateProps } from "react-router-dom";
+import { Coordinate } from "interface/ploggingInterface";
+import Swal from "sweetalert2";
 
-interface createMarker {
+interface IcreateMarker {
   latitude: number;
   longitude: number;
   map: naver.maps.Map | undefined;
@@ -9,13 +10,20 @@ interface createMarker {
   cursor?: string;
 }
 
-interface createCustomControl {
+interface IcreateCustomControl {
   html: string;
   pos: naver.maps.Position;
 }
 
-interface controlMarkers {
-  markers: naver.maps.Marker;
+interface IcreateMarkers {
+  items: Coordinate[];
+  map: naver.maps.Map | undefined;
+  url: string;
+  cursor?: string;
+}
+
+interface IcontrolMarkers {
+  markers: naver.maps.Marker[];
   map: naver.maps.Map | null;
 }
 
@@ -25,7 +33,7 @@ function createMarker({
   map,
   url,
   cursor = "pointer",
-}: createMarker): naver.maps.Marker {
+}: IcreateMarker): naver.maps.Marker {
   const marker = new naver.maps.Marker({
     position: new naver.maps.LatLng(latitude, longitude),
     map: map,
@@ -45,7 +53,7 @@ function createMarker({
 function createCustomControl({
   html,
   pos,
-}: createCustomControl): naver.maps.CustomControl {
+}: IcreateCustomControl): naver.maps.CustomControl {
   const CustomControl = new naver.maps.CustomControl(html, {
     position: pos,
   });
@@ -53,10 +61,53 @@ function createCustomControl({
   return CustomControl;
 }
 
-function attachMarkers({ markers, map }: controlMarkers) {
-  console.log(``);
+function createMarkers({
+  items,
+  map,
+  url,
+  cursor = "pointer",
+}: IcreateMarkers): naver.maps.Marker[] {
+  const markers: naver.maps.Marker[] = [];
+  items.forEach((item) => {
+    const marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(item.latitude, item.longitude),
+      map: map,
+      icon: {
+        url: url,
+        size: new naver.maps.Size(35, 35),
+        scaledSize: new naver.maps.Size(35, 35),
+        origin: new naver.maps.Point(0, 0),
+        anchor: new naver.maps.Point(17.5, 17.5),
+      },
+      cursor: cursor,
+    });
+    markers.push(marker);
+  });
+
+  // Help의 경우
+  if (items[0]?.image) {
+    markers.forEach((marker, index) => {
+      naver.maps.Event.addListener(marker, "click", () => {
+        Swal.fire({
+          imageUrl: items[index].image,
+          imageWidth: `70vw`,
+          imageHeight: `auto`,
+          imageAlt: `helpImage`,
+          text: items[index].context,
+          showConfirmButton: false,
+          showCloseButton: true,
+        });
+      });
+    });
+  }
+
+  return markers;
 }
 
-function detachMarkers({ markers, map }: controlMarkers) {
-  console.log(``);
+function controlMarkers({ markers, map }: IcontrolMarkers): void {
+  markers.forEach((marker) => {
+    marker.setMap(map);
+  });
 }
+
+export { createMarker, createCustomControl, createMarkers, controlMarkers };
