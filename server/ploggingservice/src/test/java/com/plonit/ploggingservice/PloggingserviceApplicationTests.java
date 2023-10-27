fa8551fa97758d5c8958931e01abdf40d7f12cf0
소수현 <ssuhyeon98@gmail.com>
2023-10-27 14:53:44 +0900

    [FEAT] plogging-service test 목업 작성 #S09P31C207-138

---
+++ server/ploggingservice/src/test/java/com/plonit/ploggingservice/PloggingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plonit.ploggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloggingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
