2abb8d1a54b8815ad75077cea0ceb6dbf020cf5e
박성민 <smn1196@naver.com>
2023-11-01 15:47:19 +0900

    [FEAT] 크루페이지 생성

--- client/src/pages/CrewListPage.tsx
+++ client/src/pages/CrewListPage.tsx
@@ -3,17 +3,49 @@ import { BasicTopBar } from "components/common/TopBar";
 import CustomTab from "components/common/CustomTab";
 import MyCrewList from "components/CrewList/MyCrewList";
 import TotalCrewList from "components/CrewList/TotalCrewList";
+import { Icon } from "@iconify/react";
+import style from "styles/css/CrewListPage.module.css";
+import { useNavigate } from "react-router-dom";
+
+type IconProps = {
+  onClick: (event: React.MouseEvent<SVGSVGElement>) => void;
+};
 
 const CrewListPage = () => {
+  const navigate = useNavigate();
+
   const tabProps = {
     나의크루목록: <MyCrewList />,
     전체크루목록: <TotalCrewList />,
+  };
+
+  const goCrewcreate = () => {
+    navigate("/crew/create");
   };
 
   return (
     <div>
       <BasicTopBar text="크루 목록" />
       <CustomTab tabProps={tabProps} />
+
+      <div>
+        <h1>나 여깄어.</h1>
+      </div>
+
+      <div className={style.plus}>
+        <Icon
+          icon="bi:plus-lg"
+          style={{
+            width: "2.5rem",
+            height: "2.5rem",
+            color: "white",
+            marginTop: "0.25rem",
+          }}
+        />
+      </div>
+      <div>
+        <button onClick={goCrewcreate}>button</button>
+      </div>
     </div>
   );
 };
--- client/src/pages/CrewMemberListPage.tsx
+++ client/src/pages/CrewMemberListPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { BackTopBar } from "components/common/TopBar";
+import MemberList from "components/CrewList/MemberList";
 const CrewMemberListPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BackTopBar text="멤버 목록 " />
+      <MemberList />
+    </div>
+  );
 };
 
 export default CrewMemberListPage;
