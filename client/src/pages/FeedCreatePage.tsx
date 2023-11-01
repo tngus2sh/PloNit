0c6082b3e4fdce1d75f785a64cbe799099430078
장혜원 <mins0104@naver.com>
2023-11-01 11:35:27 +0900

    [FEAT] 피드생성 레이아웃 설정

--- client/src/pages/FeedCreatePage.tsx
+++ client/src/pages/FeedCreatePage.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { BackTopBar } from "components/common/TopBar";
+import CommonButton from "components/common/CommonButton";
+import style from "styles/css/FeedCreate.module.css";
+import { Icon } from "@iconify/react";
 
 const FeedCreatePage = () => {
-  return <div>피드 생성...</div>;
+  return (
+    <div>
+      <BackTopBar text="피드 생성" />
+      <div className={style.photo}>
+        <Icon icon="bi:camera" style={{ width: "3rem", height: "2.5rem" }} />
+        <div>사진을 등록해주세요</div>
+      </div>
+      <div className={style.introduce}>
+        <textarea
+          className={style.inputBox}
+          name="feed_introduce"
+          id="feed_introduce"
+          placeholder="내용을 작성해주세요"
+        ></textarea>
+      </div>
+      <CommonButton
+        text="피드 작성하기"
+        styles={{
+          backgroundColor: "#2cd261",
+        }}
+      />
+    </div>
+  );
 };
 
 export default FeedCreatePage;
