1a3553a418ca482787fe5ccfa67cf5cc711cc10e
장혜원 <mins0104@naver.com>
2023-11-03 17:36:43 +0900

    [FEAT] 닉네임 중복 체크 설정

--- client/src/api/lib/auth.tsx
+++ client/src/api/lib/auth.tsx
@@ -19,3 +19,21 @@ export async function login(
     .then(success)
     .catch(fail);
 }
+
+export async function nicknameCheck(
+  nickname: string,
+  success: (
+    res: AxiosResponse<any, any>,
+  ) =>
+    | AxiosResponse<any, any>
+    | PromiseLike<AxiosResponse<any, any>>
+    | null
+    | undefined
+    | void,
+  fail: (err: any) => PromiseLike<never> | null | undefined | void,
+) {
+  await customApi("/plonit-service/auth")
+    .get(`/check-nickname/${nickname}`)
+    .then(success)
+    .catch(fail);
+}
