5c3386cc41c73a58b4b28c14fa6174f757533077
소수현 <ssuhyeon98@gmail.com>
2023-11-03 16:47:49 +0900

    [FEAT] 시도, 군구, 동 정보 반환 #S09P31C207-215

---
+++ server/plonitservice/src/main/java/com/plonit/plonitservice/api/region/controller/response/DongRes.java
@@ -0,0 +1,13 @@
+package com.plonit.plonitservice.api.region.controller.response;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class DongRes {
+    
+    private Long dongCode;
+    
+    private Long dongName;
+}
