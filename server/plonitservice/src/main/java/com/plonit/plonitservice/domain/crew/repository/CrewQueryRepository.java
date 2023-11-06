92582d3d4c60f2d60671956d849719ef2c666a0d
jeonghun98 <wjdgns7040@gmail.com>
2023-11-06 16:52:13 +0900

    [MODIFY] 크루, 피드 DTO 변경

--- server/plonitservice/src/main/java/com/plonit/plonitservice/domain/crew/repository/CrewQueryRepository.java
+++ server/plonitservice/src/main/java/com/plonit/plonitservice/domain/crew/repository/CrewQueryRepository.java
@@ -1,7 +1,9 @@
 package com.plonit.plonitservice.domain.crew.repository;
 
+import com.plonit.plonitservice.api.crew.controller.response.FindCrewRes;
 import com.plonit.plonitservice.api.crew.controller.response.FindCrewsRes;
 import com.plonit.plonitservice.domain.crew.Crew;
+import com.plonit.plonitservice.domain.crew.CrewMember;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
@@ -35,12 +37,30 @@ public class CrewQueryRepository {
                 .orderBy(crew.createdDate.asc())
                 .fetch();
     }
-    public Optional<Crew> findCrew(Long memberId, Long crewId) {
+    public Optional<Crew> findCrewWithMember(Long memberId, Long crewId) {
         return Optional.ofNullable(queryFactory
                 .select(crewMember.crew)
                 .from(crewMember)
                 .join(crewMember.crew, crew)
                 .where(crewMember.member.id.eq(memberId), crewMember.crew.id.eq(crewId))
+                .fetchOne());
+    }
+
+    public Optional<FindCrewRes> findCrewWithCrewMember(Long crewId) {
+        return Optional.ofNullable(queryFactory
+                .select(Projections.constructor(FindCrewRes.class,
+                        crew.id,
+                        crew.name,
+                        crew.cntPeople,
+                        crew.crewImage,
+                        crew.region,
+                        crew.introduce,
+                        crew.notice,
+                        crewMember.member.profileImage.as("crewMasterProfileImage"),
+                        crewMember.member.nickname.as("crewMasterNickname")))
+                .from(crew)
+                .join(crewMember).on(crewMember.crew.eq(crew)).fetchJoin()
+                .where(crewMember.crew.id.eq(crewId), crewMember.isCrewMaster.isTrue())
                 .fetchOne());
     }
 }
