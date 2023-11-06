319429c653309874622fdf810bf8d37a572d3c93
장혜원 <mins0104@naver.com>
2023-11-06 22:18:28 +0900

    [FIX] 크루상세 이동경로 수정

--- client/src/components/CrewList/CrewItem.tsx
+++ client/src/components/CrewList/CrewItem.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import style from "styles/css/CrewList/CrewListItem.module.css";
 import { CrewInterface } from "interface/crewInterface";
 
 const CrewItem = ({ crew }: { crew: CrewInterface }) => {
   const navigate = useNavigate();
+  const { crewId } = useParams();
   const goCommunity = () => {
-    navigate("/crew/community");
+    navigate(`/crew/community/${crewId}`);
   };
 
   return (
