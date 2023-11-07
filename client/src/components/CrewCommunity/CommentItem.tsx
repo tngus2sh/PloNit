d476a6756fc17fd0fdef15262b4b71f1b44197c9
장혜원 <mins0104@naver.com>
2023-11-08 01:41:50 +0900

    [FEAT]피드 댓글 조회 작성 설정

--- client/src/components/CrewCommunity/CommentItem.tsx
+++ client/src/components/CrewCommunity/CommentItem.tsx
@@ -1,16 +1,44 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Icon } from "@iconify/react";
 import style from "styles/css/CrewCommunityPage/CommentModal.module.css";
+import { getCommentDelete } from "api/lib/feed";
+import { CommentInterface } from "interface/crewInterface";
 
-const CommentItem = () => {
+const CommentItem = ({ comment }: { comment: CommentInterface }) => {
+  const accessToken = useSelector((state: any) => state.user.accessToken);
+  const handleDeleteComment = () => {
+    if (comment.commentId) {
+      getCommentDelete(
+        accessToken,
+        comment.commentId,
+        (res) => {
+          console.log(res.data);
+          console.log("댓글 삭제 성공");
+        },
+        (err) => {
+          console.log("댓글 삭제 에러", err);
+        },
+      );
+    }
+  };
   return (
     <div className={style.comment_item}>
-      <div className={style.comment_img}>
-        <img src="/metamong.png" alt="몽" />
+      <div className={style.right}>
+        <div className={style.comment_img}>
+          <img src={comment.profileImage} alt="프로필" />
+        </div>
+        <div className={style.comment_text}>
+          <div className={style.nickname}>{comment.nickname}</div>
+          <div className={style.content}>{comment.content}</div>
+        </div>
       </div>
-      <div className={style.comment_text}>
-        <div className={style.nickname}>HAMSTER</div>
-        <div className={style.content}>너무 즐거워 보여 하하하하하하하</div>
-      </div>
+      <Icon
+        icon="bi:x"
+        style={{ width: "1.8rem", height: "1.8rem" }}
+        className={style.x_icon}
+        onClick={handleDeleteComment}
+      />
     </div>
   );
 };
--- client/src/components/CrewCommunity/CrewFeed.tsx
+++ client/src/components/CrewCommunity/CrewFeed.tsx
@@ -40,3 +40,18 @@ const CrewFeed = () => {
 };
 
 export default CrewFeed;
+
+// import React from "react";
+// import FeedItem from "./FeedItem";
+
+// const CrewFeed = () => {
+//   return (
+//     <div>
+//       <FeedItem />
+//       <FeedItem />
+//       <div style={{ height: "4rem" }}></div>
+//     </div>
+//   );
+// };
+
+// export default CrewFeed;
