10131d0e030f71c6b072260703bd88bd5084e3d0
소수현 <ssuhyeon98@gmail.com>
2023-11-07 11:55:48 +0900

    [REFACTOR] 로그 디버깅 #S09P31C207-215

--- server/ploggingservice/src/main/java/com/plonit/ploggingservice/api/plogging/controller/SidoGugunFeignClient.java
+++ server/ploggingservice/src/main/java/com/plonit/ploggingservice/api/plogging/controller/SidoGugunFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "plonit-service")
+@FeignClient(name = "region-service", url = "https://k9c207.p.ssafy.io")
 public interface SidoGugunFeignClient {
 
     @GetMapping("/api/plonit-service/na/region/code/{sido-name}/{gugun-name}")
