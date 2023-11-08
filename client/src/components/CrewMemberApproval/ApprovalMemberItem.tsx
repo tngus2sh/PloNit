055e2322379dd8a1f8f995037312e793169e9836
장혜원 <mins0104@naver.com>
2023-11-08 23:26:05 +0900

    [FEAT] 크루 가입 승인 대기 API

---
+++ client/src/components/CrewMemberApproval/ApprovalMemberItem.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { MemberInterface } from "interface/crewInterface";
+import style from "styles/css/CrewMemberApprovalPage.module.css";
+import { getCrewAllow } from "api/lib/crew";
+
+const ApprovalMemberItem = ({ member }: { member: MemberInterface }) => {
+  const accessToken = useSelector((state: any) => state.user.accessToken);
+  const { crewId } = useParams();
+  const onRegisterApproval = (status: boolean) => {
+    const data = {
+      crewId: Number(crewId),
+      memberId: member.crewMemberId,
+      status: status,
+    };
+    getCrewAllow(
+      accessToken,
+      data,
+      (res) => {
+        console.log("크루 가입 승인 성공");
+      },
+      (err) => {
+        console.log("크루 가입 승인 실패", err);
+      },
+    );
+  };
+
+  return (
+    <div className={style.Approval_member_Item}>
+      <div className={style.left}>
+        <img src={member.profileImage} alt="Crew Image" />
+        <div className={style.nickname}>{member.nickname}</div>
+      </div>
+      <div className={style.right}>
+        <div className={style.allow} onClick={() => onRegisterApproval(true)}>
+          수락
+        </div>
+        <div className={style.refuse} onClick={() => onRegisterApproval(false)}>
+          거절
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApprovalMemberItem;
