d7c39e2f0a4cb749aebbc2ec82da527b8d4111c4
jeonghun98 <wjdgns7040@gmail.com>
2023-11-08 15:13:20 +0900

    [REFACTOR] getMemberId request 제거

--- server/ploggingservice/src/main/java/com/plonit/ploggingservice/common/util/RequestUtils.java
+++ server/ploggingservice/src/main/java/com/plonit/ploggingservice/common/util/RequestUtils.java
@@ -1,4 +1,7 @@
 package com.plonit.ploggingservice.common.util;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 import javax.servlet.http.HttpServletRequest;
 public class RequestUtils {
     public static Long getMemberKey(HttpServletRequest request) {
@@ -7,5 +10,23 @@ public class RequestUtils {
             return null;
         }
         return Long.parseLong(headerValue);
+    }
+
+    public static Long getMemberId() {
+        Long memberKey = null;
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes();
+
+        String headerValue = attributes.getRequest().getHeader("memberKey");
+        if (headerValue != null) {
+            try {
+                memberKey = Long.parseLong(headerValue);  // Ensure it's a valid Long
+            } catch (NumberFormatException e) {
+                // Handle the case where user-id is not a valid Long
+                System.err.println("memberKey header is not a valid Long: " + memberKey);
+            }
+        }
+        return memberKey;
     }
 }
