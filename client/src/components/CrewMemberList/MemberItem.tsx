9f4bbf33b32af0737b1a0c4d24248a9b1768e056
장혜원 <mins0104@naver.com>
2023-11-08 21:57:45 +0900

    [FEAT] 크루원 조회 (크루장)

--- client/src/components/CrewMemberList/MemberItem.tsx
+++ client/src/components/CrewMemberList/MemberItem.tsx
@@ -3,19 +3,27 @@ import { Icon } from "@iconify/react";
 import style from "styles/css/CrewMemberListPage.module.css";
 import { MemberInterface } from "interface/crewInterface";
 
-const MemberItem = ({ member }: { member: MemberInterface }) => {
+const MemberItem = ({
+  member,
+  master,
+}: {
+  member: MemberInterface;
+  master: boolean;
+}) => {
   return (
     <div className={style.Member_Item}>
       <img src={member.profileImage} alt="Crew Image" />
       <div className={style.nickname}>{member.nickname}</div>
-      <Icon
-        icon="bi:x"
-        style={{
-          width: "3rem",
-          height: "3rem",
-        }}
-        className={style.x_Icon}
-      />
+      {master && (
+        <Icon
+          icon="bi:x"
+          style={{
+            width: "3rem",
+            height: "3rem",
+          }}
+          className={style.x_Icon}
+        />
+      )}
     </div>
   );
 };
--- client/src/pages/CrewMemberListPage.tsx
+++ client/src/pages/CrewMemberListPage.tsx
@@ -3,32 +3,67 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { BackTopBar } from "components/common/TopBar";
 import MemberItem from "components/CrewMemberList/MemberItem";
-import { MemberInterface } from "interface/crewInterface";
-import { getCrewMember, getCrewMaster } from "api/lib/crew";
+import { CrewInterface, MemberInterface } from "interface/crewInterface";
+import { getCrewDetail, getCrewMember, getCrewMaster } from "api/lib/crew";
 
 const CrewMemberListPage = () => {
   const accessToken = useSelector((state: any) => state.user.accessToken);
   const { crewId } = useParams();
+  const [isCrewDetail, setCrewDetail] = useState<CrewInterface>(
+    {} as CrewInterface,
+  );
   const [isMemberList, setMemberList] = useState<MemberInterface[]>([]);
+
   useEffect(() => {
-    getCrewMember(
+    getCrewDetail(
       accessToken,
       Number(crewId),
       (res) => {
-        console.log(res.data);
-        setMemberList(res.data.resultBody);
-        console.error("크루원 목록조회 성공");
+        console.log("크루 상세 조회 성공");
+        setCrewDetail(res.data.resultBody);
+        if (!isCrewDetail.isCrewMaster) {
+          getCrewMember(
+            accessToken,
+            Number(crewId),
+            (res) => {
+              console.log(res.data);
+              setMemberList(res.data.resultBody);
+              console.error("크루원 목록조회 성공");
+            },
+            (err) => {
+              console.error("크루원 목록조회 실패", err);
+            },
+          );
+        } else {
+          getCrewMaster(
+            accessToken,
+            Number(crewId),
+            (res) => {
+              console.log(res.data);
+              setMemberList(res.data.resultBody);
+              console.error("크루장 목록조회 성공");
+            },
+            (err) => {
+              console.error("크루장 목록조회 실패", err);
+            },
+          );
+        }
       },
       (err) => {
-        console.error("크루원 목록조회 실패", err);
+        console.log("크루 상세 조회 실패", err);
       },
     );
   }, []);
+
   return (
     <div>
       <BackTopBar text="멤버 목록 " />
       {isMemberList.map((member, index) => (
-        <MemberItem key={index} member={member} />
+        <MemberItem
+          key={index}
+          member={member}
+          master={isCrewDetail.isCrewMaster || false}
+        />
       ))}
     </div>
   );
