b7627b0e301a3a6bb66b31128025d94027d785af
이진하 <ljh8190@naver.com>
2023-11-09 15:35:13 +0900

    [FEAT] 유저별 플로깅 횟수 반환 API

--- server/plonitservice/src/main/java/com/plonit/plonitservice/api/member/controller/PloggingFeignClient.java
+++ server/plonitservice/src/main/java/com/plonit/plonitservice/api/member/controller/PloggingFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name = "plogging-service", url = "https://k9c207.p.ssafy.io/api")
 public interface PloggingFeignClient {
 
-    @GetMapping("/plongging-service/v1/count")
+    @GetMapping("/plogging-service/v1/count")
     CustomApiResponse<Integer> countPlogging();
     
 }
