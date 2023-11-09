baa30c0bc3e628dbc7eec2650942a531678683ff
소수현 <ssuhyeon98@gmail.com>
2023-11-09 14:03:26 +0900

    [FEAT] Crew 배치 ItemReader, ItemProcessor, ItemWriter #S09P31C207-242

---
+++ server/plonitservice/src/main/java/com/plonit/plonitservice/common/batch/CrewAvgItemProcessor.java
@@ -0,0 +1,15 @@
+package com.plonit.plonitservice.common.batch;
+
+import com.plonit.plonitservice.domain.rank.CrewRanking;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CrewAvgItemProcessor implements ItemProcessor<String, CrewRanking> {
+    @Override
+    public CrewRanking process(String item) throws Exception {
+        return null;
+    }
+}
---
+++ server/plonitservice/src/main/java/com/plonit/plonitservice/common/batch/CrewAvgItemReader.java
@@ -0,0 +1,17 @@
+package com.plonit.plonitservice.common.batch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CrewAvgItemReader implements ItemReader<String> {
+    @Override
+    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        return null;
+    }
+}
