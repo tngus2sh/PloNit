0c4d732f7a2911bf49d48f34b78e6be322c95b6b
이진하 <ljh8190@naver.com>
2023-11-10 09:16:40 +0900

    [MODIFY] 내 정보 반환 API 수정

--- server/plonitservice/src/main/java/com/plonit/plonitservice/api/member/controller/PloggingFeignClient.java
+++ server/plonitservice/src/main/java/com/plonit/plonitservice/api/member/controller/PloggingFeignClient.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-//@FeignClient(name = "plogging-service", url = "https://k9c207.p.ssafy.io/api")
-@FeignClient(name = "plogging-service", url = "http://localhost:8000/api")
+@FeignClient(name = "plogging-service", url = "https://k9c207.p.ssafy.io/api")
 public interface PloggingFeignClient {
 
     @GetMapping("/plogging-service/v1/count")
