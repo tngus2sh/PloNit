e97e235bbd788fdf8fbb2d1c81a538e9ad693f37
박성민 <smn1196@naver.com>
2023-11-10 11:16:06 +0900

    [FEAT] add_detailpaga

--- client/src/components/CrewpingDetail/CrewpingInfo.tsx
+++ client/src/components/CrewpingDetail/CrewpingInfo.tsx
@@ -3,28 +3,31 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { Icon } from "@iconify/react";
 import style from "styles/css/CrewpingDetailPage/CrewpingInfo.module.css";
+import { CrewpingInterface } from "interface/crewInterface"; // 경로는 실제 경로에 맞게 조정해주세요.
 
-import { getCrewpingInfo } from "api/lib/crewping";
+interface CrewpingInfoProps {
+  crewping: CrewpingInterface;
+}
 
-const CrewpingInfo = () => {
-  const accessToken = useSelector((state: any) => state.user.accessToken);
-  //const [totalCrewList, setTotalCrewList] = useState<CrewInterface[]>([]);
-  const [inputValue, setInputValue] = useState<string>("");
-
+const CrewpingInfo = ({ crewping }: CrewpingInfoProps) => {
   return (
     <div className={style.crewping_info}>
       <div className={style.title}>활동 내용</div>
       <div className={style.content}>
         <Icon icon="bi:calendar-check" className={style.icon} />
-        <div>10.27(금) 17:00 ~ 18:30</div>
+        <div>
+          {crewping.startDate} ~ {crewping.endDate}
+        </div>
       </div>
       <div className={style.content}>
         <Icon icon="bi:geo-alt" className={style.icon} />
-        <div>수완</div>
+        <div>{crewping.place}</div>
       </div>
       <div className={style.content}>
         <Icon icon="bi:person-fill" className={style.icon} />
-        <div>5/8 명</div>
+        <div>
+          {crewping.cntPeople}/{crewping.maxPeople} 명
+        </div>
       </div>
     </div>
   );
