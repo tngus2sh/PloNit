e0dafd3372280e8cadd026e21917113191874331
박성민 <smn1196@naver.com>
2023-11-10 12:06:32 +0900

    [FEAT]add_detail

--- client/src/pages/CrewpingDetailPage.tsx
+++ client/src/pages/CrewpingDetailPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { BackTopBar } from "components/common/TopBar";
 import CrewpingLeader from "components/CrewpingDetail/CrewpingLeader";
@@ -6,17 +6,36 @@ import CrewpingInfo from "components/CrewpingDetail/CrewpingInfo";
 import CrewpingIntroduce from "components/CrewpingDetail/CrewpingIntroduce";
 import CommonButton from "components/common/CommonButton";
 import { CrewpingInterface } from "interface/crewInterface"; // 경로는 실제 경로에 맞게 조정해주세요.
+import { useSelector, useDispatch } from "react-redux";
+import { getCrewpingInfo } from "api/lib/crewping";
 
 const CrewpingDetailPage = () => {
+  const accessToken = useSelector((state: any) => state.user.accessToken);
   const location = useLocation();
-  const { crewping } = location.state || {}; // state가 없는 경우를 대비해 기본값 설정
+  const { crewpingId } = location.state || {}; // state가 없는 경우를 대비해 기본값 설정
+  const [isCrewpingInfo, setCrewpingInfo] = useState<CrewpingInterface>();
+
+  useEffect(() => {
+    getCrewpingInfo(
+      accessToken,
+      Number(crewpingId),
+      (res) => {
+        console.log("크루핑 상세 조회 성공");
+        console.log(res.data);
+        setCrewpingInfo(res.data.resultBody);
+      },
+      (err) => {
+        console.log("크루핑 상세 조회 실패", err);
+      },
+    );
+  }, []);
 
   return (
     <div>
-      <BackTopBar text={crewping.name} />
-      <CrewpingLeader crewping={crewping} />
-      <CrewpingInfo crewping={crewping} />
-      <CrewpingIntroduce crewping={crewping} />
+      {isCrewpingInfo && <BackTopBar text={isCrewpingInfo.name} />}
+      {isCrewpingInfo && <CrewpingLeader crewping={isCrewpingInfo} />}
+      {isCrewpingInfo && <CrewpingInfo crewping={isCrewpingInfo} />}
+      {isCrewpingInfo && <CrewpingIntroduce crewping={isCrewpingInfo} />}
       <CommonButton
         text="크루핑 참여"
         styles={{
