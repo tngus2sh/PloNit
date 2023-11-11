fbe511735728f7c4ddf42ebca38e5311a87301f7
장혜원 <mins0104@naver.com>
2023-11-11 21:44:23 +0900

    [FIX] 크루상세 -> 크루 커뮤니티

--- client/src/pages/CrewDetailPage.tsx
+++ client/src/pages/CrewDetailPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import { userActions } from "store/user-slice";
 import { BackTopBar } from "components/common/TopBar";
 import CrewLeader from "components/CrewDetail/CrewLeader";
 import CrewInfo from "components/CrewDetail/CrewInfo";
@@ -12,10 +11,8 @@ import { getCrewDetail, getCrewQuit } from "api/lib/crew";
 import { CrewInterface } from "interface/crewInterface";
 
 const CrewDetailPage = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const accessToken = useSelector((state: any) => state.user.auth.accessToken);
-  const User = useSelector((state: any) => state.user);
   const { crewId } = useParams();
   console.log(crewId);
   const [isCrewDetail, setCrewDetail] = useState<CrewInterface>(
@@ -30,13 +27,6 @@ const CrewDetailPage = () => {
         console.log("크루 상세 조회 성공");
         console.log(res.data);
         setCrewDetail(res.data.resultBody);
-        const data = {
-          isMyCrew: res.data.resultBody.isMyCrew,
-          isCrewMaster: res.data.resultBody.isCrewMaster,
-          isCrewwpingMaster: User.crewinfo.isCrewwpingMaster,
-        };
-        console.log(data);
-        dispatch(userActions.myCrewHandler(data));
       },
       (err) => {
         console.log("크루 상세 조회 실패", err);
@@ -59,7 +49,7 @@ const CrewDetailPage = () => {
       },
     );
   };
-  console.log(User);
+
   return (
     <div>
       <BackTopBar text={isCrewDetail.name} />
