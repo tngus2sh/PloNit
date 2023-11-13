184deb14abdbdcc56fa870e23e2d0110d9fc2b1c
tngus2sh <ssuhyeon98@gmail.com>
2023-11-14 08:42:31 +0900

    [REFACTOR] 배지 feign client 다시 설정 #S09P31C207-138

--- server/ploggingservice/src/main/java/com/plonit/ploggingservice/api/plogging/controller/BadgeFeignClient.java
+++ server/ploggingservice/src/main/java/com/plonit/ploggingservice/api/plogging/controller/BadgeFeignClient.java
@@ -4,11 +4,14 @@ package com.plonit.ploggingservice.api.plogging.controller;
 import com.plonit.ploggingservice.api.plogging.controller.request.GrantMemberBadgeReq;
 import com.plonit.ploggingservice.common.CustomApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "badge-service", url = "https://k9c207.p.ssafy.io/api")
 public interface BadgeFeignClient {
 
+    @PostMapping("/plonit-service/api/badge/member")
     CustomApiResponse<Void> grantMemberBadge(@RequestBody GrantMemberBadgeReq grantMemberBadgeReq);
 
 }
