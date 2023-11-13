71e8f77e96eef4b34dd61718d4f6ca4d2426dc96
park-js515 <hdcom37@gmail.com>
2023-11-13 14:42:04 +0900

    [TEST] worker TEST

--- client/src/workers/worker.js
+++ client/src/workers/worker.js
@@ -1,12 +1,19 @@
 self.onmessage = (event) => {
   if (event.data === "start") {
     setInterval(() => {
+      console.log("work1 is work");
       self.postMessage("tick");
     }, 1000);
   }
   if (event.data === "start2") {
+    console.log("worker2 is work");
     setInterval(() => {
       self.postMessage("tick2");
     }, 2000);
+  }
+  if (event.data === "start30") {
+    setInterval(() => {
+      self.postMessage("tick30");
+    }, 30000);
   }
 };
