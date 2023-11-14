37f1152460d3768a959323c8bf76ae2a5f1eea9c
park_js515 <hdcom37@gmail.com>
2023-11-14 21:48:47 +0900

    [CHORE] console.log 제거

--- client/src/workers/worker.js
+++ client/src/workers/worker.js
@@ -1,19 +1,16 @@
 self.onmessage = (event) => {
   if (event.data === "start") {
     setInterval(() => {
-      console.log("worker1");
       self.postMessage("tick");
     }, 1000);
   }
   if (event.data === "start2") {
     setInterval(() => {
-      console.log("worker2");
       self.postMessage("tick2");
     }, 2000);
   }
   if (event.data === "start30") {
     setInterval(() => {
-      console.log("worker3");
       self.postMessage("tick30");
     }, 30000);
   }
