45b24c702389635696c4689e3b9c431f91f9f423
jinha <ljh8190@naver.com>
2023-11-16 01:05:38 +0900

    [FEAT] 플로깅 리스트 디자인

--- client/src/components/MyPlogging/MyPloggingItem.tsx
+++ client/src/components/MyPlogging/MyPloggingItem.tsx
@@ -3,6 +3,11 @@ import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import style from "styles/css/MyPloggingPage/MyPloggingItem.module.css";
 import { PloggingLog } from "interface/ploggingInterface";
+import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
+import FmdGoodRoundedIcon from "@mui/icons-material/FmdGoodRounded";
+import AccessAlarmRoundedIcon from "@mui/icons-material/AccessAlarmRounded";
+
+import testImg from "../../image/test.jpg";
 
 const formattedDateTime = (datestr: any) => {
   const days = ["일", "월", "화", "수", "목", "금", "토"];
@@ -18,6 +23,27 @@ const formattedDateTime = (datestr: any) => {
   }${minute}`;
 
   return formattedDate;
+};
+
+const findDayOfWeek = (datestr: any) => {
+  const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const date = new Date(datestr);
+
+  return days[date.getDay()];
+};
+
+const findDay = (datestr: any) => {
+  const date = new Date(datestr);
+
+  return date.getDate();
+};
+
+const findTime = (datestr: any) => {
+  const date = new Date(datestr);
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+
+  return `${hour}시 ${minute}분`;
 };
 
 const formatMinutes = (minutes: any) => {
@@ -37,29 +63,58 @@ const MyPloggingItem = ({ plogging }: { plogging: PloggingLog }) => {
   return (
     <div className={style.myplogging_item} onClick={goPloggingDetailHandler}>
       <div className={style.left_area}>
-        <img src={User.info.profileImage} alt="프로필" />
-      </div>
-      <div className={style.right_area}>
-        <div className={style.first_area}>
-          {plogging.type === "IND" ? (
-            <div className={style.divide_IND}>개인</div>
-          ) : null}
-          {plogging.type === "VOL" ? (
-            <div className={style.divide_VOL}>봉사</div>
-          ) : null}
-          {plogging.type === "CREWPING" ? (
-            <div className={style.divide_CREWPING}>크루</div>
-          ) : null}
-          <div className={style.date_place}>
-            {formattedDateTime(plogging.startTime)} {plogging.place}
+        {/* <img src={User.info.profileImage} alt="프로필" /> */}
+        {/* <img src={testImg} alt="프로필" /> */}
+        <div className={style.week_container}>
+          <div>{findDayOfWeek(plogging.startTime)}</div>
+          <div className={style.day_container}>
+            {findDay(plogging.startTime)}
           </div>
         </div>
-        <div className={style.second_area}>
-          <div className={style.dist}>
+      </div>
+      <div style={{ width: "1rem" }}>
+        <div className={style.v_line}></div>
+      </div>
+      <div className={style.right_area}>
+        <div className={style.info_container}>
+          <div className={style.type_container}>
+            {plogging.type === "IND" ? (
+              <div className={`${style.type} ${style.type_IND}`}>개인</div>
+            ) : null}
+            {plogging.type === "VOL" ? (
+              <div className={`${style.type} ${style.type_VOL}`}>봉사</div>
+            ) : null}
+            {plogging.type === "CREWPING" ? (
+              <div className={`${style.type} ${style.type_CREWPING}`}>크루</div>
+            ) : null}
+          </div>
+
+          <div className={style.place_container}>
+            <FmdGoodRoundedIcon sx={{ fontSize: "1.1rem" }} />
+            &nbsp;{plogging.place}
+          </div>
+          {/* <div className={style.date_container}>
+            <CalendarMonthRoundedIcon sx={{ fontSize: "1.1rem" }} />
+            &nbsp;{formattedDateTime(plogging.startTime)}
+          </div> */}
+
+          {/* <div className={style.date_container}>
+            <AccessAlarmRoundedIcon sx={{ fontSize: "1.1rem" }} />
+            &nbsp;{findTime(plogging.startTime)}
+          </div> */}
+        </div>
+
+        {/* <div className={style.place_container}>
+          <FmdGoodRoundedIcon sx={{ fontSize: "1.1rem" }} />
+          &nbsp;{plogging.place}
+        </div> */}
+
+        <div className={style.plogging_container}>
+          <div className={style.distance_container}>
             거리 <span className={style.largeNumber}>{plogging.distance}</span>
             km
           </div>
-          <div className={style.time}>
+          <div className={style.time_container}>
             시간{" "}
             <span className={style.largeNumber}>
               {formatMinutes(plogging.totalTime)}
